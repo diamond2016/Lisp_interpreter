da0e2d6996377610ac5432de9a38ca5919cea9c0
diamond2016 <riccardogcorsi@gmail.com>
2025-07-19 17:51:17 +0200

    Version as chapter 4

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Build Lisp interpreter
 This is a project from original idea of https://buildyouorwnlisp.com 
 
-I follow lessons on Youtute channel esadecimale](https://www.youtube.com/@esadecimale).
+I follow lessons on Youtube channel [esadecimale](https://www.youtube.com/@esadecimale).
 
 **Language C**
 
 **Libraries**
-TODO
+linenoise - simple C library to read/write on terminal of antirez [linenoise] (http://github.com/antirez/linenoise)
 
 
---
+++ repl.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "linenoise/linenoise.h"
+
+int main(int argc, char** argv) {
+
+  /* Print Version and Exit Information */
+  puts("Lispy Version 0.0.0.0.1");
+  puts("Press Ctrl+c to Exit\n");
+
+  /* In a never ending loop */
+  while (1) {
+    char *line = NULL;
+
+    /* Output our prompt and get input */
+    if ((line = linenoise("lispy> ")) == NULL) {
+      exit(1);
+    }
+
+    /* Add input to history */
+    linenoiseHistoryAdd(line);
+
+    /* Echo input back to user */
+    printf("No you're a %s\n", line);
+
+    /* Free retrieved input */
+    linenoiseFree(line);
+
+  }
+
+  return 0;
+}
