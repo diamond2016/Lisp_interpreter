ab68a5c01aeefb9b62793731f15a6afcbe1d40b9
diamond2016 <riccardogcorsi@gmail.com>
2025-07-25 17:02:48 +0200

    change history management in repl

--- repl.c
+++ repl.c
@@ -2,11 +2,18 @@
 #include <stdlib.h>
 #include "linenoise/linenoise.h"
 
+#define HISTORY_LEN   1024
+
 int main(int argc, char** argv) {
 
   /* Print Version and Exit Information */
   puts("Lispy Version 0.0.0.0.1");
   puts("Press Ctrl+c to Exit\n");
+
+  if (linenoiseHistorySetMaxLen(HISTORY_LEN) == 0) {
+      fprintf(stderr, "could not set history buffer, exiting...");
+      exit(1);
+  }
 
   /* In a never ending loop */
   while (1) {
@@ -14,11 +21,15 @@ int main(int argc, char** argv) {
 
     /* Output our prompt and get input */
     if ((line = linenoise("lispy> ")) == NULL) {
+      fprintf(stderr, "could not read input line, exiting...");
       exit(1);
     }
 
     /* Add input to history */
-    linenoiseHistoryAdd(line);
+    if ((linenoiseHistoryAdd(line)) == 0) {
+      fprintf(stderr, "could not add line to history, exiting...");
+      exit(1);
+    };
 
     /* Echo input back to user */
     printf("No you're a %s\n", line);
