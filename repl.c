f09e5ca8b50e8d6f88d250f7bdf4736c455b95aa
diamond2016 <riccardogcorsi@gmail.com>
2025-07-26 16:08:00 +0200

    update repl

--- repl.c
+++ repl.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "linenoise/linenoise.h"
+#include "mpc/mpc.h"
 
 #define HISTORY_LEN   1024
 
@@ -15,29 +16,44 @@ int main(int argc, char** argv) {
       exit(1);
   }
 
+  /* create some parsers */
+  mpc_parser_t *Number  = mpc_new("number");
+  mpc_parser_t *Operator  = mpc_new("operator");
+  mpc_parser_t *Expr = mpc_new("expr");
+  mpc_parser_t *Lispy = mpc_new("lispy");
+
+  /* regex between \/ and with ; at end*/
+  mpca_lang(MPCA_LANG_DEFAULT,
+    " number : /-?[0-9]+/; "
+    " operator : '+' | '-' | '*' | '/'; "
+    " expr : <number> | '(' <operator> <expr>+ ')'; "
+    " lispy : /^/ <expr>+ /$/; ",
+    Number, Operator, Expr, Lispy);
+
   /* In a never ending loop */
   while (1) {
-    char *line = NULL;
-
-    /* Output our prompt and get input */
-    if ((line = linenoise("lispy> ")) == NULL) {
-      fprintf(stderr, "could not read input line, exiting...");
-      exit(1);
-    }
+    char *input = linenoise("lispy> ");
 
     /* Add input to history */
-    if ((linenoiseHistoryAdd(line)) == 0) {
+    if ((linenoiseHistoryAdd(input)) == 0) {
       fprintf(stderr, "could not add line to history, exiting...");
       exit(1);
     };
 
-    /* Echo input back to user */
-    printf("No you're a %s\n", line);
-
+    // parsing
+    mpc_result_t r;
+    if (mpc_parse("<stdin>", input, Lispy, &r)) {
+      //print AST
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    } else {
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
+ 
     /* Free retrieved input */
-    linenoiseFree(line);
-
+    linenoiseFree(input);
   }
-
+  mpc_cleanup(4, Number, Operator, Expr, Lispy);
   return 0;
 }
